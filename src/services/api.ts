80954f4d637e747b5a7f624e412c3a3ee8cee4e5
Andrea Jonus <j.jonus.j@gmail.com>
2020-05-21 19:32:06 +1000

    Add basic API definition

---
+++ src/services/api.ts
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+export default axios.create({
+  baseURL: 'http://localhost:3000',
+  headers: {
+    post: {
+      'Content-Type': 'application/json',
+    },
+  },
+});
---
+++ src/types.ts
@@ -0,0 +1,6 @@
+export interface Device {
+  id: String;
+  name: String | null;
+  fan: boolean;
+  light: boolean;
+}
